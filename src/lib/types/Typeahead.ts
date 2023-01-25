a3def831803a0412eb897720ce900e36f509c240
Timothy Trowbridge <timothy.trowbridge@neolution.ch>
2023-01-25 11:01:57 +0200

    Default Selected Option for Typeaheads

--- src/lib/types/Typeahead.ts
+++ src/lib/types/Typeahead.ts
@@ -1,3 +1,12 @@
+import { FieldValues } from "react-hook-form";
+import { CommonInputProps } from "./CommonInputProps";
 import { LabelValueOption } from "./LabelValueOption";
 
 export type TypeaheadOptions = LabelValueOption[] | string[];
+
+interface CommonTypeaheadProps<T extends FieldValues> extends CommonInputProps<T> {
+  multiple?: boolean;
+  defaultSelected?: TypeaheadOptions;
+}
+
+export { CommonTypeaheadProps };
