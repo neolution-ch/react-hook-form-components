6539ae457135aa9af8a2971a16b3cc3fbfec403b
neotrow <timothy.trowbridge@neolution.ch>
2023-05-12 18:28:35 +0200

    added test for text input to auto save

--- cypress/cypress/component/AutoSubmit/AutoSumbit.cy.tsx
+++ cypress/cypress/component/AutoSubmit/AutoSumbit.cy.tsx
@@ -1,7 +1,8 @@
 import { faker } from "@faker-js/faker";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Form, Input, LabelValueOption } from "react-hook-form-components";
+import { Form, Input } from "react-hook-form-components";
 import * as yup from "yup";
+import { generateOptions } from "../../helpers/typeahead";
 
 it("radio button multiple autosave works", () => {
   const name1 = faker.random.alpha(10);
@@ -13,8 +14,8 @@ it("radio button multiple autosave works", () => {
     [name2]: yup.string(),
   });
 
-  const options1 = faker.helpers.uniqueArray<LabelValueOption>(() => ({ value: faker.random.alpha(10), label: faker.random.alpha(10) }), 3);
-  const options2 = faker.helpers.uniqueArray<LabelValueOption>(() => ({ value: faker.random.alpha(10), label: faker.random.alpha(10) }), 3);
+  const { objectOptions: options1 } = generateOptions(faker.datatype.number({ min: 1, max: 10 }));
+  const { objectOptions: options2 } = generateOptions(faker.datatype.number({ min: 1, max: 10 }));
 
   cy.mount(
     <>
@@ -46,7 +47,7 @@ it("radio button autosave only once", () => {
     [name]: yup.string(),
   });
 
-  const options = faker.helpers.uniqueArray<LabelValueOption>(() => ({ value: faker.random.alpha(10), label: faker.random.alpha(10) }), 3);
+  const { objectOptions: options } = generateOptions(faker.datatype.number({ min: 1, max: 10 }));
 
   cy.mount(
     <Form onSubmit={cy.spy().as("onSubmitSpy")} resolver={yupResolver(schema)} autoSubmitConfig={{ wait: 500 }}>
@@ -55,8 +56,30 @@ it("radio button autosave only once", () => {
   );
 
   cy.get("@onSubmitSpy").should("have.callCount", 0);
+
   options.forEach(({ label }) => {
     cy.contains("label", label).click();
   });
-  cy.get("@onSubmitSpy").should("be.calledOnceWith", { [name]: options[2].value });
+
+  cy.get("@onSubmitSpy").should("be.calledOnceWith", { [name]: options[options.length - 1].value });
+  cy.get("@onSubmitSpy").should("have.callCount", 1);
+});
+
+it("text input autosave only once", () => {
+  const name = faker.random.alpha(10);
+  const schema = yup.object().shape({
+    [name]: yup.string(),
+  });
+
+  const randomText = faker.random.alphaNumeric(10);
+
+  cy.mount(
+    <Form onSubmit={cy.spy().as("onSubmitSpy")} resolver={yupResolver(schema)} autoSubmitConfig={{ wait: 500 }}>
+      <Input type="text" label={name} name={name} />
+    </Form>,
+  );
+
+  cy.contains("label", name).click().type(randomText);
+  cy.get("@onSubmitSpy").should("be.calledOnceWith", { [name]: randomText });
+  cy.get("@onSubmitSpy").should("have.callCount", 1);
 });
