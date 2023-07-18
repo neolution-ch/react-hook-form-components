84a416730cec83305f9d20f25967e55591184a5a
Timothy Trowbridge <timothy.trowbridge@neolution.ch>
2023-07-18 15:33:44 +0200

    allow up to 3 retries

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   component: {
+    retries: {
+      openMode: 0,
+      runMode: 2,
+    },
     devServer: {
       framework: "next",
       bundler: "webpack",
