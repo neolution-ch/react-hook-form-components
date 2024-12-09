481a84aa49641a5e228b9a9dc92e3a0535b5c494
Gianmarco Manni <gianmarco.manni@neolution.ch>
2024-12-09 12:32:41 +0100

    x

--- fix-autosuggest-highlight.js
+++ fix-autosuggest-highlight.js
@@ -4,13 +4,12 @@ const path = require("path");
 
 const typesFilePath = path.resolve("node_modules/@types/autosuggest-highlight/index.d.ts");
 
-// Define the new content you want to inject into the file
-const newContent = `
+const dtsFakeContent = `
 declare module 'autosuggest-highlight' {
-  // Your type declarations or empty declaration if needed
-  export function someFunction(): any; // Or add any other types you need
+  // Declare a dummy function to let ts compile the module
+  export function dummyFunction(): void;
 }
 `;
 
 // Check if the file exists, and write new content if it does
-fs.writeFileSync(typesFilePath, newContent, "utf8");
+fs.writeFileSync(typesFilePath, dtsFakeContent, "utf8");
